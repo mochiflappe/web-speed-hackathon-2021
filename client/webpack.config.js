e7a53442886db0c5c8031bd1b8cb4a5b1e98b229
mochiflappe <mochiflappe@gmail.com>
2022-01-03 09:22:30 +0900

    ビルド時はenvとwebpackのmodeをproductionに、ポリフィルも必要なブラウザを指定

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ const config = {
     },
     static: [PUBLIC_PATH, UPLOAD_PATH],
   },
-  devtool: 'inline-source-map',
+  devtool: process.env.NODE_ENV === 'development' ? 'inline-source-map' : false,
   entry: {
     main: [
       'core-js',
@@ -64,7 +64,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
+      NODE_ENV: process.env.NODE_ENV,
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
